2dd342df8ef22f670fb29e96d3e5b3d5e1beb768
Rosen Yorgov <yorgov.h.r@gmail.com>
2025-07-13 01:49:19 +0300

    gameplay page styling wip

--- fe/src/views/GamePlay.tsx
+++ fe/src/views/GamePlay.tsx
@@ -1,8 +1,8 @@
 import { alphabetLetters, getCharGrid } from "../utils/Utils.ts";
 import Button from "../components/Button.tsx";
-import "./GamePlay.scss";
 import { type GridOfLetters, type AlphabetLetter } from "../types/Types.ts";
 import { useEffect, useState, type ReactNode } from "react";
+import "./GamePlay.scss";
 
 const GamePlay = () => {
   //TODO: get the selected category from the global state
@@ -10,7 +10,9 @@ const GamePlay = () => {
   //show a loading spinner while fetching the word
   const [guessedLetters, setGuessedLetters] = useState<AlphabetLetter[]>([]);
   const [allLetters, setAllLetters] = useState<GridOfLetters>([]);
-  const [allClickedLetters, setAllClickedLetters] = useState<AlphabetLetter[]>([]);
+  const [allClickedLetters, setAllClickedLetters] = useState<AlphabetLetter[]>(
+    [],
+  );
 
   useEffect(() => {
     if (allLetters.length) return;
@@ -55,17 +57,21 @@ const GamePlay = () => {
   const renderAllWordsCharacterPlaceholders = (): ReactNode => {
     return allLetters.map((chars, i) => (
       <div key={i} className="GamePlay__main__words__word">
-        {
-          chars.map((char, j) => {
-            const guessed: boolean = isLetterGuessed(char);
+        {chars.map((char, j) => {
+          const guessed: boolean = isLetterGuessed(char);
 
-            return (
-              <p key={`${i}-${j}`} className={`GamePlay__main__words__word__letter${!guessed && '--inactive'}`}>
-                {guessed ? char : ""}
-              </p>
-            )
-          })
-        }
+          return (
+            <p
+              key={`${i}-${j}`}
+              className={`GamePlay__main__words__word__letter GamePlay__main__words__word__letter${!guessed && "--inactive"}`}
+            >
+              {guessed ? char : ""}
+            </p>
+          );
+        })}
+        {i < allLetters.length - 1 && (
+          <div className="GamePlay__main__words__word__spacer" />
+        )}
       </div>
     ));
   };
@@ -77,14 +83,16 @@ const GamePlay = () => {
         <div className="GamePlay__header__lives"></div>
       </header>
       <main className="GamePlay__main">
-        <div className="GamePlay__main__words">{renderAllWordsCharacterPlaceholders()}</div>
+        <div className="GamePlay__main__words">
+          {renderAllWordsCharacterPlaceholders()}
+        </div>
         <div className="GamePlay__main__alphabet">
           {alphabetLetters.map((letter) => {
             return (
               <Button
                 disabled={isLetterClicked(letter)}
                 text={letter}
-                classNames={`GamePlay__main__alphabet__letter-btn${isLetterClicked(letter) ? '--disabled' : ''}`}
+                classNames={`GamePlay__main__alphabet__letter-btn ${isLetterClicked(letter) ? "GamePlay__main__alphabet__letter-btn--disabled" : ""}`}
                 clickHandler={() => handleLetterClick(letter)}
               />
             );
