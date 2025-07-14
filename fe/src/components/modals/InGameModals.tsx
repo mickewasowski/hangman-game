1f0a8a53b1c412411ef24214bc2f07346f1c84ee
Rosen Yorgov <yorgov.h.r@gmail.com>
2025-07-14 23:34:06 +0300

    paused modal wip

--- fe/src/components/modals/InGameModals.tsx
+++ fe/src/components/modals/InGameModals.tsx
@@ -1,5 +1,6 @@
-import { BaseModal } from "./BaseModal";
-
+import { BaseModal } from "./BaseModal.tsx";
+import Button from "../Button.tsx";
+import "./InGameModals.scss";
 
 //TODO: the below should have one common class InGameModal
 //this class should provide the basic structure of the component:
@@ -8,30 +9,36 @@ import { BaseModal } from "./BaseModal";
 //3. position
 //4. backdrop
 
-
 const PauseModal = () => {
-
   return (
-  <BaseModal header={<></>} classNames="" children={<></>} />
-  )
+    <BaseModal
+      backdrop={true}
+      header={
+        <div className="PauseModal__header">
+          <h3 className="PauseModal__header__title" data-text="Paused">Paused</h3>
+        </div>
+      }
+      classNames="PauseModal"
+      children={
+        <div className="PauseModal__main">
+          <Button classNames="PauseModal__main__button" text="continue" />
+          <Button classNames="PauseModal__main__button" text="new category" />
+          <Button
+            classNames="PauseModal__main__button PauseModal__main__button--quit"
+            text="quit game"
+          />
+        </div>
+      }
+    />
+  );
 };
 
 const WinModal = () => {
-
-  return (
-  <BaseModal header={<></>} classNames="" children={<></>} />
-  )
+  return <BaseModal header={<></>} classNames="" children={<></>} />;
 };
 
 const LooseModal = () => {
-
-  return (
-  <BaseModal header={<></>} classNames="" children={<></>} />
-  )
+  return <BaseModal header={<></>} classNames="" children={<></>} />;
 };
 
-export {
-  PauseModal,
-  WinModal,
-  LooseModal
-};
+export { PauseModal, WinModal, LooseModal };
